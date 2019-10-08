743d2c785c064edcd41c50ed2123a38ef8a9e575
Nathan Martindale <nathanamartindale@gmail.com>
2019-10-07 20:07:52 -0500

    Add MNIST and PUBG prediction notebooks

--- PUBG/notebooks/util.py
+++ PUBG/notebooks/util.py
@@ -4,3 +4,7 @@ import pandas as pd
 def load_train_data():
     df = pd.read_csv("../data/raw/train_V2.csv")
     return df
+
+def load_test_data():
+    df = pd.read_csv("../data/raw/test_V2.csv")
+    return df
